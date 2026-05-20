bbdec79d41c05cb77d4bc225912aaf727423a1f4
kasi4ril <fly2kasi@gmail.com>
2026-05-20 14:39:51 +0100

    More Skills

---
+++ Printif.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Instructions
+//Write a function that takes a string as an argument and returns the letter G followed by a newline \n
+// if the argument length is more or equal than 3, otherwise returns Invalid Input followed by a newline \n.
+//If it's an empty string return G followed by a newline \n.
+
+func PrintIf(str string) string {
+
+	if len(str) >= 3 {
+		return "G\n"
+	}
+	if str == "" {
+		return "G\n"
+	}
+	return "Invalid\n"
+}
+
+func main() {
+	fmt.Print(PrintIf("abcdefz"))
+	fmt.Print(PrintIf("abc"))
+	fmt.Print(PrintIf(""))
+	fmt.Print(PrintIf("14"))
+}
+
+//output
+//$ go run . | cat -e
+//G$
+//G$
+//G$
+//Invalid Input$
